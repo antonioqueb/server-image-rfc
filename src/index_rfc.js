22728c1be802f20d42c85736fd02ca1f6ca5e0aa
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-07 14:03:41 -0600

    chore: Configurar CORS para aceptar solicitudes solo de https://historiallaboral.com/

--- src/index_rfc.js
+++ src/index_rfc.js
@@ -1,15 +1,26 @@
-// src/index_rfc.js
 const express = require('express');
+const cors = require('cors');
 const uploadRFCRouter = require('./routes/upload_rfc');
 
 const app = express();
 const port = 3010;
 
+// Configurar CORS para aceptar solicitudes solo de https://historiallaboral.com/
+const corsOptions = {
+  origin: 'https://historiallaboral.com',
+  optionsSuccessStatus: 200 // Para compatibilidad con navegadores más antiguos
+};
+
+app.use(cors(corsOptions));
+
+// Configurar body parsers
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Rutas
 app.use('/upload_rfc', uploadRFCRouter);
 
+// Iniciar servidor
 app.listen(port, () => {
   console.log(`RFC Server is running at http://149.50.128.198:${port}`);
 });
